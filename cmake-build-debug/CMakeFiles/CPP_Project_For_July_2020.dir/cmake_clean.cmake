ae811ce35b8d55f50a7b6d83eeddc674665b820c
辛宁 张 <474381670@qq.com>
2020-07-02 21:58:31 +0800

    initial commit
    initial commit

---
+++ cmake-build-debug/CMakeFiles/CPP_Project_For_July_2020.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CPP_Project_For_July_2020.dir/main.cpp.obj"
+  "CPP_Project_For_July_2020.exe"
+  "CPP_Project_For_July_2020.exe.manifest"
+  "CPP_Project_For_July_2020.pdb"
+  "libCPP_Project_For_July_2020.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CPP_Project_For_July_2020.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
